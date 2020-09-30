4dedbadd8fa2ef95ae9fb4b05aca0752c8164310
nbenlin <nihatbenli.nb@gmail.com>
2020-09-30 14:48:03 +0200

    created layouts

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">Footer &copy; 2020</div>
+            <div class="col-md-6 text-right"><a href="https://github.com/nbenlin">Github</a></div>
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomepageController@index')->name('homepage');
