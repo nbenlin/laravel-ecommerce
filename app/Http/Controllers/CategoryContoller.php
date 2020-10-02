6edf42b69ecdb9ecafc834e9320db8b6b2fa3612
nbenlin <nihatbenli.nb@gmail.com>
2020-10-02 11:13:49 +0200

    .

--- app/Http/Controllers/CategoryContoller.php
+++ app/Http/Controllers/CategoryContoller.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class CategoryContoller extends Controller
 {
     public function index() {
-        return view('kategorie');
+        return view('category');
     }
 }
