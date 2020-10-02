397eb6788b622f42421b3636b597c3f870277de7
nbenlin <nihatbenli.nb@gmail.com>
2020-10-02 12:29:29 +0200

    .

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CategoryContoller extends Controller
+class CategoryController extends Controller
 {
     public function index() {
         return view('category');
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,74 +1,176 @@
 @extends('layouts.master')
 @section('title', 'Kategori')
 @section('content')
-<div class="container">
-    <ol class="breadcrumb">
-       <li><a href="#">Anasayfa</a></li>
-       <li><a href="#">Kategori</a></li>
-       <li class="active">Kategori</li>
-   </ol>
+<div class="container mt-4">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Strona Główna</a></li>
+          <li class="breadcrumb-item"><a href="#">Kategoria</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Kategoria</li>
+        </ol>
+      </nav>
    <div class="row">
        <div class="col-md-3">
-           <div class="panel panel-default">
-               <div class="panel-heading">Kategori Adı</div>
-               <div class="panel-body">
-                   <h3>Alt Kategoriler</h3>
-                   <div class="list-group categories">
-                       <a href="#" class="list-group-item"><i class="fa fa-television"></i> Alt Kategori</a>
-                       <a href="#" class="list-group-item"><i class="fa fa-television"></i> Alt Kategori</a>
-                       <a href="#" class="list-group-item"><i class="fa fa-television"></i> Alt Kategori</a>
-                   </div>
-                   <h3>Fiyat Aralığı</h3>
-                   <form>
-                       <div class="form-group">
-                           <div class="checkbox">
-                               <label>
-                                   <input type="checkbox"> 100-200
-                               </label>
-                           </div>
-                       </div>
-                       <div class="form-group">
-                           <div class="checkbox">
-                               <label>
-                                   <input type="checkbox"> 200-300
-                               </label>
-                           </div>
-                       </div>
-                   </form>
+           <div class="card">
+               <div class="card-header">Kategori Adı</div>
+               <div class="card-body">
+                   <div class="card">
+                        <div class="card-header">Alt Kategoriler</div>
+                        <div class="card-body">
+                            <div class="list-group">
+                                <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-minus mr-1"></i>Alt Kategori</a>
+                                <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-minus mr-1"></i>Alt Kategori</a>
+                                <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-minus mr-1"></i>Alt Kategori</a>
+                                <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-minus mr-1"></i>Alt Kategori</a>
+                                <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-minus mr-1"></i>Alt Kategori</a>
+                                <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-minus mr-1"></i>Alt Kategori</a>                 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mt-3">
+                        <div class="card-header">Przedział cenowy</div>
+                        <div class="card-body">
+                            <div class="list-group categories">
+                                <form>
+                                    <div class="form-group">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox"> 100-200
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox"> 200-300
+                                            </label>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>                    
                </div>
            </div>
        </div>
        <div class="col-md-9">
-           <div class="products bg-content">
-               Sırala
-               <a href="#" class="btn btn-default">Çok Satanlar</a>
-               <a href="#" class="btn btn-default">Yeni Ürünler</a>
-               <hr>
-               <div class="row">
-                   <div class="col-md-3 product">
-                       <a href="#"><img src="http://lorempixel.com/400/400/food/1"></a>
-                       <p><a href="#">Ürün adı</a></p>
-                       <p class="price">129 ₺</p>
-                       <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
-                   </div>
-                   <div class="col-md-3 product">
-                       <a href="#"><img src="http://lorempixel.com/400/400/food/2"></a>
-                       <p><a href="#">Ürün adı</a></p>
-                       <p class="price">129 ₺</p>
-                       <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
-                   </div>
-                   <div class="col-md-3 product">
-                       <a href="#"><img src="http://lorempixel.com/400/400/food/3"></a>
-                       <p><a href="#">Ürün adı</a></p>
-                       <p class="price">129 ₺</p>
-                       <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
-                   </div>
-                   <div class="col-md-3 product">
-                       <a href="#"><img src="http://lorempixel.com/400/400/food/4"></a>
-                       <p><a href="#">Ürün adı</a></p>
-                       <p class="price">129 ₺</p>
-                       <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
-                   </div>
+        <div class="products">
+            <div class="card card-theme">
+                <div class="card-header">
+                    Sortuj
+                    <a class="btn btn-light border" href="#" role="button">Popularny</a>
+                    <a class="btn btn-light border" href="#" role="button">Nowości</a>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3 product">
+                            <div class="card border-0">
+                                <img class="card-img-top" src="{{ asset('img/promocja.jpg') }}" alt="Card image cap">
+                                <div class="card-body">
+                                    <p class="card-text"><h6 class="card-title font-weight-bold">Bebetto KIDS</h6>Noworodek bałewniany komplet.</p>
+                                    <div class="card-price">
+                                        <p class="card-text"><s>79,99 PLN</s></p>
+                                        <p class="price-text">39,99 PLN</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 product">
+                            <div class="card border-0">
+                                <img class="card-img-top" src="{{ asset('img/promocja.jpg') }}" alt="Card image cap">
+                                <div class="card-body">
+                                    <p class="card-text"><h6 class="card-title font-weight-bold">Bebetto KIDS</h6>Noworodek bałewniany komplet.</p>
+                                    <div class="card-price">
+                                        <p class="card-text"><s>79,99 PLN</s></p>
+                                        <p class="price-text">39,99 PLN</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 product">
+                            <div class="card border-0">
+                                <img class="card-img-top" src="{{ asset('img/promocja.jpg') }}" alt="Card image cap">
+                                <div class="card-body">
+                                    <p class="card-text"><h6 class="card-title font-weight-bold">Bebetto KIDS</h6>Noworodek bałewniany komplet.</p>
+                                    <div class="card-price">
+                                        <p class="card-text"><s>79,99 PLN</s></p>
+                                        <p class="price-text">39,99 PLN</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 product">
+                            <div class="card border-0">
+                                <img class="card-img-top" src="{{ asset('img/promocja.jpg') }}" alt="Card image cap">
+                                <div class="card-body">
+                                    <p class="card-text"><h6 class="card-title font-weight-bold">Bebetto KIDS</h6>Noworodek bałewniany komplet.</p>
+                                    <div class="card-price">
+                                        <p class="card-text"><s>79,99 PLN</s></p>
+                                        <p class="price-text">39,99 PLN</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                <div class="row">
+                    <div class="col-md-3 product">
+                        <div class="card border-0">
+                            <img class="card-img-top" src="{{ asset('img/promocja.jpg') }}" alt="Card image cap">
+                            <div class="card-body">
+                                <p class="card-text"><h6 class="card-title font-weight-bold">Bebetto KIDS</h6>Noworodek bałewniany komplet.</p>
+                                <div class="card-price">
+                                    <p class="card-text"><s>79,99 PLN</s></p>
+                                    <p class="price-text">39,99 PLN</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 product">
+                        <div class="card border-0">
+                            <img class="card-img-top" src="{{ asset('img/promocja.jpg') }}" alt="Card image cap">
+                            <div class="card-body">
+                                <p class="card-text"><h6 class="card-title font-weight-bold">Bebetto KIDS</h6>Noworodek bałewniany komplet.</p>
+                                <div class="card-price">
+                                    <p class="card-text"><s>79,99 PLN</s></p>
+                                    <p class="price-text">39,99 PLN</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 product">
+                        <div class="card border-0">
+                            <img class="card-img-top" src="{{ asset('img/promocja.jpg') }}" alt="Card image cap">
+                            <div class="card-body">
+                                <p class="card-text"><h6 class="card-title font-weight-bold">Bebetto KIDS</h6>Noworodek bałewniany komplet.</p>
+                                <div class="card-price">
+                                    <p class="card-text"><s>79,99 PLN</s></p>
+                                    <p class="price-text">39,99 PLN</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 product">
+                        <div class="card border-0">
+                            <img class="card-img-top" src="{{ asset('img/promocja.jpg') }}" alt="Card image cap">
+                            <div class="card-body">
+                                <p class="card-text"><h6 class="card-title font-weight-bold">Bebetto KIDS</h6>
+                                    Noworodek bałewniany komplet
+                                </p>
+                                <div class="card-price">
+                                    <p class="card-text"><s>79,99 PLN</s></p>
+                                    <p class="price-text">39,99 PLN</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+            </div>
+        </div> 
+        </div>
+    </div>
                </div>
            </div>
        </div>
